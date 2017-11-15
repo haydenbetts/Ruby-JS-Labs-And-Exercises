c69ef45631335b44ae8811dc3eeb83358590069f
Hayden Betts <haydenbetts@gmail.com>
2017-11-15 03:30:01 +0000

    reservation instance methods

--- rails/associations_and_rails/flatiron-bnb-methods-cb-gh-000/app/models/reservation.rb
+++ rails/associations_and_rails/flatiron-bnb-methods-cb-gh-000/app/models/reservation.rb
@@ -6,17 +6,47 @@ class Reservation < ActiveRecord::Base
 
   # activerecord validations
   validates_presence_of :checkin, :checkout
-  validates :status, presence: true
-  before_create :guest_does_not_own_listing
+  validate :guest_does_not_own_listing, :ci_before_co
   # validates :guest, presence: true
   # validates :listing, presence: true
   # validates :review, presence: true
   
+  include Reservable
+  
+  
+  def duration
+    date_parser(self.checkout.to_s) - date_parser(self.checkin.to_s)
+  end
+  
+  def total_price
+    self.duration * self.listing.price
+  end
+  
+  
   private
     
-    # I should do custom validation right?
-    def guest_does_not_own_listing
-      self.guest != self.listing.host
+  def guest_does_not_own_listing
+    
+    # when writing custom validations, you need to add an error. You cannot just
+    # return true or false...
+    if self.guest == self.listing.host
+      errors.add(:guest_id, "You can't book your own apartment")
     end
+  end
+  
+  def listing_available
+  end
+  
+  def ci_before_co
+    
+      checkin = date_parser(self.checkin.to_s)
+      checkout = date_parser(self.checkout.to_s)
+    
+    if checkin > checkout
+      errors.add(:checkin, "Your checkin date must be before your checkout date")
+    elsif checkin == checkout
+      errors.add(:checkin, "Your checkin and checkout dates must be different")
+    end
+  end
 
 end
