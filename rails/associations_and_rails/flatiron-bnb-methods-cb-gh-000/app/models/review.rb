7195d94fd58f441f0362192383bec16bf3f2dba8
Hayden Betts <haydenbetts@gmail.com>
2017-11-16 04:22:23 +0000

    review tests passing

--- rails/associations_and_rails/flatiron-bnb-methods-cb-gh-000/app/models/review.rb
+++ rails/associations_and_rails/flatiron-bnb-methods-cb-gh-000/app/models/review.rb
@@ -3,32 +3,23 @@ class Review < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   
   validates :rating, :description, presence: :true
-  # validate :reservation_accepted
-  validate :has_reservation, :reservation_accepted
+  validate :reservation_accepted
+  #validate :has_reservation, :reservation_accepted
   
   # make sure that Reservation.status == "accepted"
   
   private
   
-    # def reservation_accepted
-    #   if !self.reservation
-    #     errors.add(:reservation_id, "You must have a reservation")
-    #   elsif self.reservation.status != "accepted"
-    #     errors.add(:reservation_id, "You can only leave review if your reservation was accepted")
-    #   end
-    # end
-     
-    def has_reservation
-      if !self.reservation
-         errors.add(:reservation_id, "You must have a reservation")
-      end
-    end
-     
+    # in this function I take advantage of the fact that teach part of this if
+    # block runs in order. TO make this more robust you should not rely on that...
     def reservation_accepted
-      if self.reservation.status != "accepted"
-         errors.add(:reservation_id, "You can only leave review if your reservation was accepted")
+      if !self.reservation
+        errors.add(:reservation_id, "You must have a reservation")
+      elsif self.reservation.status != "accepted"
+        errors.add(:reservation_id, "You can only leave review if your reservation was accepted")
+      elsif self.reservation.checkout > Date.today
+        errors.add(:reservation_id, "You must have already checkout to leave a review")
       end
     end
-  
   
 end
