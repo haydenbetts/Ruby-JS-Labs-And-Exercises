f714ef54e216725c482d94c0422946e1e63cbc99
haydenbetts <haydenbetts@gmail.com>
2017-11-19 14:59:46 -0800

    forms-and-basic-associations-lab form half-built

--- rails/associations_and_rails/forms-and-basic-associations-rails-lab-cb-gh-000/app/models/song.rb
+++ rails/associations_and_rails/forms-and-basic-associations-rails-lab-cb-gh-000/app/models/song.rb
@@ -2,16 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  
+
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name: name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
-  
+
   def artist_name
     if self.artist
       self.artist.name
     end
   end
-  
-end
 
+end
