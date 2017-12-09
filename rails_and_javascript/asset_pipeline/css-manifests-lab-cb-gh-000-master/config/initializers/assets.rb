2493f80c1c806c83feed703533192dda6beea3b5
haydenbetts <haydenbetts@gmail.com>
2017-12-09 11:34:03 -0800

    css manifests lab

--- rails_and_javascript/asset_pipeline/css-manifests-lab-cb-gh-000-master/config/initializers/assets.rb
+++ rails_and_javascript/asset_pipeline/css-manifests-lab-cb-gh-000-master/config/initializers/assets.rb
@@ -4,7 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.precompile += %w( learn.js )
+# you need to add new css manifests to this file but... not new js manifests...
+Rails.application.config.assets.precompile += %w( learn.js admin.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
