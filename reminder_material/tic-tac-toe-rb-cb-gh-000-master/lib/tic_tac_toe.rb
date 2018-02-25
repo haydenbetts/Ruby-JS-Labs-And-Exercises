71323c16fccfd74a93b4a4e6e6938488510a790e
haydenbetts <haydenbetts@gmail.com>
2018-02-25 10:50:57 -0800

    half-way done

--- reminder_material/tic-tac-toe-rb-cb-gh-000-master/lib/tic_tac_toe.rb
+++ reminder_material/tic-tac-toe-rb-cb-gh-000-master/lib/tic_tac_toe.rb
@@ -1,5 +1,56 @@
+require 'pry'
+
 WIN_COMBINATIONS = [
 ]
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+player = 1
+
+def greeting(board, player)
+  puts "Welcome to Tic Tac Toe!"
+  game_loop(board, player)
+end
+
+def game_loop(board, player)
+  display_board(board)
+  input = get_input(player)
+
+  if valid_move?(board, input)
+    # for now I am just going to try updating the board
+    if player == 1
+      board[input] = "X"
+      player = 2
+      game_loop(board, player)
+    else
+      board[input] = "O"
+      player = 1
+      game_loop(board, player)
+    end
+  else
+    game_loop(board, player)
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def get_input(player)
+  puts "Enter your move Player #{player}"
+  gets.strip.to_i
+end
+
+def valid_move?(board, input)
+  if board[input] == " "
+    true
+  else
+    false
+  end
+end
 
 # 1. Display greeting
 # 2. display board
@@ -16,3 +67,5 @@ WIN_COMBINATIONS = [
   # check your array against the win conditions array.
   # if it matches any of them, congrats the winner
   # if not, ask for input from the next player
+
+greeting(board, player)
