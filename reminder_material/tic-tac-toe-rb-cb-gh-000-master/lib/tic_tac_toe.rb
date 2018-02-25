b65ff2d01fe74c7f93bf13249c5e9cbef8bcb2fb
haydenbetts <haydenbetts@gmail.com>
2018-02-25 10:24:00 -0800

    my ttt plan

--- reminder_material/tic-tac-toe-rb-cb-gh-000-master/lib/tic_tac_toe.rb
+++ reminder_material/tic-tac-toe-rb-cb-gh-000-master/lib/tic_tac_toe.rb
@@ -1,1 +1,18 @@
+WIN_COMBINATIONS = [
+]
 
+# 1. Display greeting
+# 2. display board
+# 3. Ask for input from player x
+  # call the validate function on the new input
+# 4. Determine if input is valid
+  # 4a if valid, and if player 1, insert x
+    # call the win condition function on updated array
+  # 4b if valid, and if player 2, insert o
+    # call the win condition function on updated array
+  # 4c if not valid, run the ask for input function again
+
+# 5. Determine if the game is over.
+  # check your array against the win conditions array.
+  # if it matches any of them, congrats the winner
+  # if not, ask for input from the next player
